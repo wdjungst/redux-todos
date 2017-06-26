d27f60627d4dbc98e51f9ae0150bf52cecf83329
David Jungst <djungst@gmail.com>
2017-06-26 10:32:09 -0600

    redux added

---
+++ src/reducers/todos.js
@@ -0,0 +1,12 @@
+const todos = ( state = [], action ) => {
+  switch (action.type ) {
+    case 'TODOS':
+      return action.todos;
+    case 'ADD_TODO':
+      return [action.todo, ...state]
+    default:
+      return state;
+  }
+}
+
+export default todos;
